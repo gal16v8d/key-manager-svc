8d9af905bf883ac44b94f18091c8452587a87319
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-10-11 19:44:44 -0500

    Update spring-boot version

--- src/main/java/co/com/gsdd/keymanager/configs/SecurityConfig.java
+++ src/main/java/co/com/gsdd/keymanager/configs/SecurityConfig.java
@@ -19,29 +19,33 @@ import co.com.gsdd.keymanager.services.UsuarioService;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UsuarioService userService;
-    @Autowired
-    private PasswordEncoder passEncoder;
+	private final UsuarioService userService;
+	private final PasswordEncoder passEncoder;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
-        auth.authenticationProvider(authenticationProvider());
-    }
+	@Autowired
+	public SecurityConfig(UsuarioService userService, PasswordEncoder passEncoder) {
+		this.userService = userService;
+		this.passEncoder = passEncoder;
+	}
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userService);
-        authenticationProvider.setPasswordEncoder(passEncoder);
-        return authenticationProvider;
-    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService);
+		auth.authenticationProvider(authenticationProvider());
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest()
-                .authenticated().and().httpBasic();
-    }
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userService);
+		authenticationProvider.setPasswordEncoder(passEncoder);
+		return authenticationProvider;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest()
+				.authenticated().and().httpBasic();
+	}
 
 }
