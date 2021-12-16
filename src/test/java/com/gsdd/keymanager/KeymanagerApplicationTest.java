0b854d7b9f2af667e166dcc2c37289d8ec93f49e
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-15 21:30:10 -0500

    Update spring-boot version, waiting springfox fix for update to 2.6.1

--- src/test/java/com/gsdd/keymanager/KeymanagerApplicationTest.java
+++ src/test/java/com/gsdd/keymanager/KeymanagerApplicationTest.java
@@ -1,13 +1,10 @@
 package com.gsdd.keymanager;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-class KeymanagerApplicationTests {
+class KeymanagerApplicationTest {
 
   @Test
   void contextLoads() {}
